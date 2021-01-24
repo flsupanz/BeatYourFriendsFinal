ec05c69021dbb822d4fd00e4420ad3c24ef868f6
floriansupanz <florian.supanz@gmail.com>
2021-01-24 18:23:47 +0100

    configure for heroku deployment

---
+++ server.js
@@ -0,0 +1,26 @@
+
+require('rootpath')();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+// api routes
+app.use('/users', require('./users/users.controller'));
+
+app.use('/quiz', require('./quiz/quiz.controller'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/WebTechProject2020'));
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/WebTechProject2020/index.html'));
+});
+
+// start server
+const port = 8080;
+app.listen(port, () => console.log('Server listening on port ' + port));
